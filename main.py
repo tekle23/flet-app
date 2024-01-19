3fc08191532e4481b3958ccfff798fd5887f009d
tekle <tekle.y@ashewa.com>
2024-01-19 10:19:15 -0500

    updated

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 import flet as ft
 
-
 def main(page: ft.Page):
-    page.title = "Hello World"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.add(ft.Text("Hello Tekle!"))
-    page.bgcolor = ft.colors.BLUE_GREY_100
+    def check_item_clicked(e):
+        e.control.checked = not e.control.checked
+        page.update()
+
+    page.appbar = ft.AppBar(
+        leading=ft.Icon(ft.icons.ACCESSIBILITY),
+        leading_width=40,
+        title=ft.Text("Flet App"),
+        center_title=False,
+        bgcolor=ft.colors.BLUE_ACCENT_400,
+        actions=[
+            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
+            ft.IconButton(ft.icons.FILTER_3),
+            ft.PopupMenuButton(
+                items=[
+                    ft.PopupMenuItem(text="Item 1"),
+                    ft.PopupMenuItem(),  # divider
+                    ft.PopupMenuItem(
+                        text="Checked item", checked=False, on_click=check_item_clicked
+                    ),
+                ]
+            ),
+        ],
+    )
     
 
-ft.app(main)
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationDestination(icon=ft.icons.EXPLORE, label="Explore"),
+            ft.NavigationDestination(icon=ft.icons.COMMUTE, label="Commute"),
+            ft.NavigationDestination(
+                icon=ft.icons.BOOKMARK_BORDER,
+                selected_icon=ft.icons.BOOKMARK,
+                label="Explore",
+            ),
+        ]
+    )
+
+    page.add(ft.Text("Body!")),
+
+ft.app(target=main)
